alumet-dev/alumet

feat(plugin-aggregation): Add the first version

plugin-aggregation/src/lib.rs
@@ -1,0 +1,59 @@
+mod transform;
+
+use std::time::Duration;
+
+use alumet::plugin::{
+    rust::{deserialize_config, serialize_config, AlumetPlugin},
+    ConfigTable,
+};
+
+use serde::{Deserialize, Serialize};
+use transform::AggregationTransform;
+
+pub struct AggregationPlugin {
+    config: Config,
+}
+
+impl AlumetPlugin for AggregationPlugin {
+    fn name() -> &'static str {
+        "aggregation"
+    }
+
+    fn version() -> &'static str {
+        env!("CARGO_PKG_VERSION")
+    }
+
+    fn default_config() -> anyhow::Result<Option<ConfigTable>> {
+        Ok(Some(serialize_config(Config::default())?))
+    }
+
+    fn init(config: ConfigTable) -> anyhow::Result<Box<Self>> {
+        let config = deserialize_config(config)?;
+        Ok(Box::new(AggregationPlugin { config }))
+    }
+
+    fn start(&mut self, alumet: &mut alumet::plugin::AlumetStart) -> anyhow::Result<()> {
+        let transform = Box::new(AggregationTransform::new(self.config.interval)?);
+        alumet.add_transform(transform);
+        Ok(())
+    }
+
+    fn stop(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    /// Interval for the aggregation.
+    #[serde(with = "humantime_serde")]
+    interval: Duration,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            interval: Duration::from_secs(60),
+        }
+    }
+}

AngeCyp/alumet-testing

plugin-aggregation/src/lib.rs
@@ -1,0 +1,59 @@
+mod transform;
+
+use std::time::Duration;
+
+use alumet::plugin::{
+    rust::{deserialize_config, serialize_config, AlumetPlugin},
+    ConfigTable,
+};
+
+use serde::{Deserialize, Serialize};
+use transform::AggregationTransform;
+
+pub struct AggregationPlugin {
+    config: Config,
+}
+
+impl AlumetPlugin for AggregationPlugin {
+    fn name() -> &'static str {
+        "aggregation"
+    }
+
+    fn version() -> &'static str {
+        env!("CARGO_PKG_VERSION")
+    }
+
+    fn default_config() -> anyhow::Result<Option<ConfigTable>> {
+        Ok(Some(serialize_config(Config::default())?))
+    }
+
+    fn init(config: ConfigTable) -> anyhow::Result<Box<Self>> {
+        let config = deserialize_config(config)?;
+        Ok(Box::new(AggregationPlugin { config }))
+    }
+
+    fn start(&mut self, alumet: &mut alumet::plugin::AlumetPluginStart) -> anyhow::Result<()> {
+        let transform = Box::new(AggregationTransform::new(self.config.interval)?);
+        alumet.add_transform(transform);
+        Ok(())
+    }
+
+    fn stop(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    /// Interval for the aggregation.
+    #[serde(with = "humantime_serde")]
+    interval: Duration,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            interval: Duration::from_secs(60),
+        }
+    }
+}